170fc7b45c064ad6e6dc21d1aaef844cbcf1327a
Christopher Smith <csmith4work@gmail.com>
2022-11-27 03:42:17 -0600

    fix: connected to db successfully.

--- SSDemo/Startup.cs
+++ SSDemo/Startup.cs
@@ -52,7 +52,7 @@ namespace SSDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if(Env.IsDevelopment())
+            if(!Env.IsDevelopment())
             {
                 using (IServiceScope scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
                 {
