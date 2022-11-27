3ecffd5fe5157cd09d7218a3c217c89a42c708ef
Christopher Smith <csmith4work@gmail.com>
2022-11-27 03:13:38 -0600

    fix: db connection string

--- SSDemo/Startup.cs
+++ SSDemo/Startup.cs
@@ -29,10 +29,11 @@ namespace SSDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var currentConnString = Env.IsDevelopment() ? "DefaultConnection" : "Server=sql;Database=LNCDemo;User=sa;Password=Demos123!;";
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString(currentConnString)));
+                options.UseSqlServer(Env.IsDevelopment() ?
+                    Configuration.GetConnectionString("DefaultConnection") :
+                    "Server=sql;Database=LNCDemo;User=sa;Password=Demos123!;"));
 
             services.AddIdentity<ApplicationUser, ApplicationRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
